d336e31808caec92c20ab936b702d3d71e49a34d
karol <karol.kruszynski0@gmail.com>
2024-06-24 13:09:53 +0200

    # Data Checking

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 import pandas as pd
 import numpy as np
+df = pd.read_excel('data.xlsx', na_filter=True)
+print(df)
+
+# Data Checking
+print(df.columns)
+print(df.info())
+print(df.describe())
+
+print(df['Driver and Team'].unique())
