c7fa2e134bc0667e8f1b55ce498723f13b67aac3
Karol Kruszyński <90861840+karolkruszynski@users.noreply.github.com>
2024-06-24 17:01:47 +0200

    data fix + new points gap col

--- main.py
+++ main.py
@@ -30,3 +30,42 @@ print(df)
 
 # Validation None data for Team column
 print(df[df['Team'].isna()])
+
+# For results we modify and insert data manually
+dittmann_racing = [19,34,5]
+for num in df['Num']:
+    if num in dittmann_racing:
+        df.loc[[18,22,25],'Team'] = "Chris Dittmann Racing"
+    else:
+        df.loc[24, 'Team'] = "Fortec Motorsports"
+
+print(df[df['Team'] == 'Dittmann Racing'])
+
+# Changing data in Driver and Team column, to only name for 18,22,24,25 indexes
+def extract_name(info):
+    # Match RegEx [XX]
+    match = re.search(r'\[.*?\]', info)
+    if match:
+        # split string after match and return first half
+        return info.split(match.group())[0]
+    return np.nan
+
+# Use function to make new column
+df['Driver'] = df['Driver and Team'].apply(extract_name)
+
+# Repair a names for Drivers with NaN
+df.loc[18,'Driver'] = "Kanato Le"
+df.loc[22, 'Driver'] = "Martinius Stenshorne"
+df.loc[25, 'Driver'] = "Javier Sagrera"
+df.loc[24, 'Driver'] = "James Wharton"
+print(df['Driver'])
+
+# Drop 'Driver and Team' cause we got split columns for Team and Driver separetly
+df.drop('Driver and Team', axis=1, inplace=True)
+print(df.columns)
+
+# Making points gap to leader column
+for driver in df['Driver']:
+    leader_points = max(df['Points'])
+    df['Points Gap To Leader'] = df['Points'].apply(lambda x: x - leader_points)
+print(df['Points Gap To Leader'])
