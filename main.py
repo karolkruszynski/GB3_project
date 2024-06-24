c80389e9713df47de31b5ee994da153b24fcc7eb
karol <karol.kruszynski0@gmail.com>
2024-06-24 13:53:14 +0200

    # Extracting Team to new Column

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+import re
+
 df = pd.read_excel('data.xlsx', na_filter=True)
 print(df)
 
@@ -9,3 +11,18 @@ print(df.info())
 print(df.describe())
 
 print(df['Driver and Team'].unique())
+print(df['Driver and Team'].values)
+
+# Extract Team
+def extract_team(info):
+    # Match RegEx [XX]
+    match = re.search(r'\[.*?\]', info)
+    if match:
+        # split string after match and return second half
+        return info.split(match.group())[1]
+    return None
+
+# Use fucntion to make new column
+df['Team'] = df['Driver and Team'].apply(extract_team)
+
+print(df['Team'])
